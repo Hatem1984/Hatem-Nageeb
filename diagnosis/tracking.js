0b3b0436d91b45d499683dcab3048c968872af1e
Hatem1984 <88743642+Hatem1984@users.noreply.github.com>
2026-09-14 18:38:11 +0300

    Move diagnosis analytics bootstrap to dedicated script

---
+++ diagnosis/tracking.js
@@ -0,0 +1,12 @@
+(function(){
+  try{
+    !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init','2667954303602754');fbq('track','PageView');fbq('track','ViewContent',{content_name:'Decision Lab'});
+  }catch(e){}
+  try{
+    window.dataLayer=window.dataLayer||[];
+    window.gtag=window.gtag||function(){dataLayer.push(arguments)};
+    var s=document.createElement('script');s.async=true;s.src='https://www.googletagmanager.com/gtag/js?id=G-C2N57VGT70';document.head.appendChild(s);
+    gtag('js',new Date());gtag('config','G-C2N57VGT70');
+  }catch(e){}
+})();
